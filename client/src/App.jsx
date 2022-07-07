5ca6869a5a629ed67bd56bb84b6fb89d6a7f5a72
Muhammed Shamal <muhammedshamal0@gmail.com>
2022-07-07 14:39:26 +0530

    begin redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { getPosts } from "./actions/posts";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./pages/About/About";
 import Auth from "./pages/Auth/Auth";
@@ -12,18 +9,9 @@ import UserProfile from "./pages/UserProfile/UserProfile";
 import "./styles.css";
 
 function App() {
-  // const dispatch = useDispatch();
-  // const posts = useSelector((state) => state.posts);
-  const getPosts = async () => {
-    const { data } = await axios.get("http://localhost:8000/posts");
-    console.log(data);
-  };
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    getPosts();
-    // dispatch(getPosts);
-    // console.log(posts);
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, []);
 
