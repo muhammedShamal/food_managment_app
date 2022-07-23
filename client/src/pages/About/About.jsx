ea73fe309235e7bca18bbbb1c6469cc886cdf305
Muhammed Shamal <muhammedshamal0@gmail.com>
2022-07-23 14:09:24 +0530

    updated about section

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -8,7 +8,9 @@ const About = () => {
         serve their multiple needs. Customers use our platform to donate or sell
         food, search for food at a cheap rate, find food donors. This is a step
         to manage excess food and reach out to those who are searching for a
-        meal at an affordable rate.
+        meal at an affordable rate. We can utilize excess food to feed hungry
+        stomachs. Be their happiness and hope. Save our planet from food
+        wastage. We dream of a better tomorrow for everyone.
       </p>
     </div>
   );
