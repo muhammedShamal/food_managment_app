17f64e15e512f9b7052219398a485c0816a6cd2f
Muhammed Shamal <muhammedshamal0@gmail.com>
2022-07-24 17:04:50 +0530

    changed proxy

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://pure-harbor-77871.herokuapp.com/",
 });
-
-// https://pure-harbor-77871.herokuapp.com/
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
